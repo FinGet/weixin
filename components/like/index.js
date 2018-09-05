c11b5c84f242aa710fb0a7d0fc5ebcd028b2ed4f
FinGet <zhangbiaoFT@163.com>
2018-09-05 18:07:46 +0800

    init

---
+++ components/like/index.js
@@ -0,0 +1,40 @@
+// components/like/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    like: Boolean,
+    count: Number,
+    readOnly: Boolean
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    yesSrc: 'images/like.png',
+    noSrc: 'images/like@dis.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onLike: function(e) {
+      if (this.properties.readOnly) {
+        return
+      }
+      let count = this.properties.count
+      count = this.properties.like ? count - 1 : count + 1
+      this.setData({
+        count: count,
+        like: !this.properties.like
+      })
+      let behavior = this.properties.like ? 'like' : 'cancel'
+      this.triggerEvent('like', {
+        behavior: behavior
+      }, {})
+    }
+  }
+})
---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  api_blink_url: 'http://bl.7yue.pro/v1/',
+  appkey: '98HcsgdJ3mx4Ufcm'
+}
+
+export { config }
